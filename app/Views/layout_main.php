67bc78fb8e9fbf15ce4853acd3b99bb59af959a1
Tom Papatolis <49147550+tompapatolis@users.noreply.github.com>
2026-02-11 18:49:19 +0100

    Small Fix

--- app/Views/layout_main.php
+++ app/Views/layout_main.php
@@ -5,11 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="base-url" content="<?=base_url();?>">
-    <title>Collections :: Own Everything and be Much Happier</title>
+    <title>Collections :: Own Everything and be Much Much Happier</title>
     <link rel="stylesheet" href="<?= base_url('css/tailwind.css?v='.SYS_VERSION) ?>">
     <link rel="stylesheet" href="<?= base_url('assets/fonts/arimo/style.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/fonts/firasans/style.css') ?>">
-    <link rel="icon" type="image/svg+xml" href="<?= base_url('gfx/favicon.svg') ?>">
+
+    <link rel="icon" type="image/svg+xml" href="<?=base_url('gfx/favicon/favicon.svg')?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?=base_url('gfx/favicon/favicon-32.png')?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?=base_url('gfx/favicon/apple-touch-icon.png')?>">
+    <link rel="icon" href="<?=base_url('gfx/favicon/favicon.ico')?>">
+
     <script src="<?=base_url('js/app-dist.js?v='.SYS_VERSION)?>" defer></script>
 </head>
 
